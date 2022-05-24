932f3e76f19e8b7edd12ca4e6f23fdedf464e9b3
Luan-Koekemoer <luankoeke@gmail.com>
2022-05-24 15:07:28 +0200

    added config script

---
+++ configure.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+<< _
+Author: Luan Koekemoer
+Files: All the configurations of the related programs I use
+Scrtip: Initialize all the configuraitons 
+Date Modified: 2022/05/24 
+_
+
+cp -r ./.config/* ~/.config/
